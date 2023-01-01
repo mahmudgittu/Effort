03f51ad82aeb41218fba2437407a0e1777edad46
mahmudgittu <mahmudhossainus@gmail.com>
2023-01-01 14:23:09 -0500

    Initial Commit

---
+++ Git_command.R
@@ -0,0 +1,10 @@
+# Git commands
+# configuring git user name and user email and confirming your confirmation
+# probably connects the git account to computer git
+git config --global user.name "Jane Doe" 
+git config --global user.email janedoe@gmail.com 
+git config --list
+# SSH/RSA Key/view public key
+#ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKxKoBlpEcxwLstfsRpyrTBGaKjdo8GXkrHPHEI/8ar3 mahmu@LAPTOP-M3F0PT14
+# opening c drive in git bash. probably this also a command for command panel
+pwd
