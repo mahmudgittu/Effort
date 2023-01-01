13ca3d8032c0de20a0bc9b1a7e6a7fe2d4eecf40
mahmudgittu <mahmudhossainus@gmail.com>
2023-01-01 14:36:53 -0500

    Connecting existing R project

--- Git_command.R
+++ Git_command.R
@@ -3,8 +3,14 @@
 # probably connects the git account to computer git
 git config --global user.name "Jane Doe" 
 git config --global user.email janedoe@gmail.com 
-git config --list
+git config --list 
 # SSH/RSA Key/view public key
 #ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKxKoBlpEcxwLstfsRpyrTBGaKjdo8GXkrHPHEI/8ar3 mahmu@LAPTOP-M3F0PT14
 # opening c drive in git bash. probably this also a command for command panel
 pwd
+# Linking existing project to Git in git bash
+cd ~/dir/name/of/path/to/file
+git init
+git add . 
+# Commit these changes to the git repository 
+git commit -m "Initial commit" 
